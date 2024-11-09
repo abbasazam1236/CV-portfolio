4bddb57696fb4cd5aef4189e9575fba350e6dc31
Muhammad Abbas Azam <abbas.051.tuf@gmail.com>
2024-11-10 00:46:24 +0500

    change technologies

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,8 +1,6 @@
 import { RiReactjsLine } from "react-icons/ri";
-import { TbBrandNextjs } from "react-icons/tb";
-import { BiLogoPostgresql } from "react-icons/bi";
-import { FaNodeJs, FaHtml5, FaCss3 } from "react-icons/fa";
-import { SiTailwindcss } from "react-icons/si";
+import { FaHtml5, FaCss3 } from "react-icons/fa";
+import { SiTailwindcss, SiMysql, SiJavascript } from "react-icons/si";
 import { motion } from "framer-motion";
 
 const iconVariants = (duration) => ({
@@ -29,57 +27,109 @@ const Technologies = () => {
         Technologies
       </motion.h1>
       <motion.div
-      whileInView={{ opacity: 1, x: 0 }}
-      initial={{ opacity: 0, x: -100 }}
-      transition={{ duration: 1.5 }}
-      className="flex flex-wrap items-center justify-center gap-4">
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
         <motion.div
           variants={iconVariants(2.5)}
           initial="initial"
           animate="animate"
-          className="rounded-2xl border-4 border-neutral-800 p-4"
+          className="relative group rounded-2xl border-4 border-neutral-800 p-4"
         >
-          <RiReactjsLine className="text-7xl text-cyan-400" />
-        </motion.div>
-        <motion.div
-          variants={iconVariants(3)}
-          initial="initial"
-          animate="animate"
-          className="rounded-2xl border-4 border-neutral-800 p-4"
-        >
-          <FaHtml5 className="text-7xl text-orange-500" />
-        </motion.div>
-        <motion.div
-          variants={iconVariants(5)}
-          initial="initial"
-          animate="animate"
-          className="rounded-2xl border-4 border-neutral-800 p-4"
-        >
-          <FaCss3 className="text-7xl text-blue-600" />
-        </motion.div>
-        <motion.div
-          variants={iconVariants(2)}
-          initial="initial"
-          animate="animate"
-          className="rounded-2xl border-4 border-neutral-800 p-4"
-        >
-          <SiTailwindcss className="text-7xl text-blue-400" />
+          <a
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="ReactJS"
+            className="flex justify-center items-center"
+          >
+            <RiReactjsLine className="text-7xl text-cyan-400" />
+          </a>
         </motion.div>
         <motion.div
           variants={iconVariants(6)}
           initial="initial"
           animate="animate"
-          className="rounded-2xl border-4 border-neutral-800 p-4"
+          className="relative group rounded-2xl border-4 border-neutral-800 p-4"
         >
-          <TbBrandNextjs className="text-7xl" />
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="JavaScript"
+            className="flex justify-center items-center"
+          >
+            <SiJavascript className="text-7xl text-yellow-500" />
+          </a>
         </motion.div>
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="relative group rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="HTML5"
+            className="flex justify-center items-center"
+          >
+            <FaHtml5 className="text-7xl text-orange-500" />
+          </a>
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="relative group rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/CSS"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="CSS3"
+            className="flex justify-center items-center"
+          >
+            <FaCss3 className="text-7xl text-blue-600" />
+          </a>
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(2)}
+          initial="initial"
+          animate="animate"
+          className="relative group rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <a
+            href="https://tailwindcss.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Tailwind CSS"
+            className="flex justify-center items-center"
+          >
+            <SiTailwindcss className="text-7xl text-blue-400" />
+          </a>
+        </motion.div>
+
         <motion.div
           variants={iconVariants(4)}
           initial="initial"
           animate="animate"
-          className="rounded-2xl border-4 border-neutral-800 p-4"
+          className="relative group rounded-2xl border-4 border-neutral-800 p-4"
         >
-          <BiLogoPostgresql className="text-7xl text-sky-700" />
+          <a
+            href="https://www.mysql.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="MySQL"
+            className="flex justify-center items-center"
+          >
+            <SiMysql className="text-7xl text-blue-600" />
+          </a>
         </motion.div>
       </motion.div>
     </div>
